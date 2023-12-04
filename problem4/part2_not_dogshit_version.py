1608e11297588374c47ae965ed933e1eaa6d2a34
Atharv Bhat <bhat.atharv@gmail.com>
2023-12-04 16:29:51 -0500

    not poo poo version part2

---
+++ problem4/part2_not_dogshit_version.py
@@ -0,0 +1,43 @@
+"""
+To run this code, you will need atleast 8 gigs of ram
+"""
+from utils import Card
+
+cards_dict: dict[str: list[Card]] = {}
+card_copies: dict[str:int] = {}
+
+
+def insert_cards(card_id: int):
+    insert_idx = card_id + 1
+    copies = cards_dict[card_id].overlap
+    for i in range(copies):
+        card_copies[insert_idx] += card_copies[card_id]
+        insert_idx += 1
+
+
+def count_cards(cards_dict):
+    count = 0
+    for card_id in cards_dict:
+        count += cards_dict[card_id]
+    return count
+
+
+def main():
+    with open("input.txt", "r") as file:
+        input_text = file.read()
+
+    input_lines = input_text.split("\n")
+
+    for line in input_lines:
+        card = Card(line)
+        cards_dict[card.card_id] = card
+        card_copies[card.card_id] = 1
+
+    for card_id in cards_dict:
+        insert_cards(card_id)
+
+    print(f"Total cards : {count_cards(card_copies)}")
+
+
+if __name__ == "__main__":
+    main()
